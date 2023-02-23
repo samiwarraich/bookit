a1d7b762e519431f1cd6501effe1223edb41fc19
Sami Warraich <samiwarraich51@gmail.com>
2023-02-23 21:31:44 +0300

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { wrapper } from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
