8363dbb803f6906960c36e3c4f315e22f795cc1e
Sami Warraich <samiwarraich51@gmail.com>
2023-06-15 12:25:45 +0300

    fixes

--- utils/seeder.js
+++ utils/seeder.js
@@ -2,15 +2,12 @@ const Room = require("../models/room");
 const mongoose = require("mongoose");
 const rooms = require("../data/rooms");
 
-mongoose.connect(
-  "mongodb+srv://admin:samiwarraich@cluster.eggyf.mongodb.net/bookit?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  }
-);
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
 const seedRooms = async () => {
   try {
