177f97eb6400cafb4507476b51a992071415079d
Sami Warraich <samiwarraich51@gmail.com>
2023-10-25 20:14:45 +0300

    change footer copyright text

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -3,9 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="py-1">
-      <p className="text-center mt-1">
-        book it. - 2019-2023, All Rights Reserved
-      </p>
+      <p className="text-center mt-1">{`© ${new Date().getFullYear()} book it.`}</p>
     </footer>
   );
 };
